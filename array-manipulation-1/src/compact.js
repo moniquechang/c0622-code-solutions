07dbfbc68cb77c68d6194c8a94d52a08febab2f9
Monique Chang <moniquechang1997@gmail.com>
2022-07-11 18:57:42 +0000

    edited compact function

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -4,16 +4,14 @@
    variable declration assinged to new, empty array (newArray)
    for loop where i is index num of parameter array
      (i = 0, i < array length, i++)
-   if statement where if elements in array do not equal falsey values of: false,
-     null, undefined, 0, NaN, and '' then array element gets pushed to new array
-       (have to use Number.isNaN() for NaN condition)
+   if statement where if elements in array are truthy values, push values into new array
    new array is returned from function after loop ends
 */
 
 function compact(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== null && array[i] !== 0 && array[i] !== undefined && array[i] !== '' && Number.isNaN(array[i]) !== true) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
