22b09b68a417c7c8b67253165d088822365ddbfb
Monique Chang <moniquechang1997@gmail.com>
2022-07-14 22:07:27 +0000

    edited function from counter to boolean

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,16 @@
 var $container = document.querySelector('.container');
 var $button = document.querySelector('button');
-var count = 0;
+var lightOn = true;
 
 function handleClick(event) {
-  count++;
-  if (count % 2 === 0) {
+  if (lightOn === true) {
     $container.className = 'container on';
     $button.className = 'button-on';
+    lightOn = false;
   } else {
     $container.className = 'container off';
     $button.className = 'button-off';
+    lightOn = true;
   }
 }
 
