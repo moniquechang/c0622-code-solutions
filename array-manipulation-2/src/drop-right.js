d3911918762881a9d5496b8bd0a10dcaf15a2892
Monique Chang <moniquechang1997@gmail.com>
2022-07-25 20:28:50 +0000

    edited 'drop' and 'drop-right' after pull request feedback

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -5,19 +5,14 @@
     variable declaration for final array to return (is empty at first)
     for loop where i is indexes of parameter array and condition statement is
       that i is less than the array.length minus the count
-    also need to take into account for empty array parameter: just return an empty array
-    otherwise push the array[i] into the final array
+    push the array[i] into the final array
     return the final array from the function
 */
 
 function dropRight(array, count) {
   var finalArr = [];
   for (var i = 0; i < array.length - count; i++) {
-    if (array.length === 0) {
-      finalArr = [];
-    } else {
-      finalArr.push(array[i]);
-    }
+    finalArr.push(array[i]);
   }
   return finalArr;
 }
--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -5,18 +5,13 @@
     variable declaration for final array to return (is empty at first)
     for loop where i is the indexes of parameter array to push items from parameter
       array into final array starting from the index number equal to that of count parameter
-    if statement to account for empty parameter arrays, to return an empty array
     return the final array from function
 */
 
 function drop(array, count) {
   var finalArr = [];
   for (var i = count; i < array.length; i++) {
-    if (array.length === 0) {
-      finalArr = [];
-    } else {
-      finalArr.push(array[i]);
-    }
+    finalArr.push(array[i]);
   }
   return finalArr;
 }
