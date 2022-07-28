3aff9aba3a421d9ff4a9e2c4b5a44dbb9fd02a8c
Monique Chang <moniquechang1997@gmail.com>
2022-07-28 17:03:57 +0000

    three problems completed

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,16 @@
 /* exported reverseWords */
+
+function reverseWords(string) {
+  var stringArr = string.split(' ');
+  var newString = '';
+  for (var i = 0; i < stringArr.length; i++) {
+    for (var k = stringArr[i].length - 1; k >= 0; k--) {
+      if (k !== 0 || (k === 0 && i === stringArr.length - 1)) {
+        newString += stringArr[i][k];
+      } else if (k === 0 && i !== stringArr.length - 1) {
+        newString += stringArr[i][k] + ' ';
+      }
+    }
+  }
+  return newString;
+}
