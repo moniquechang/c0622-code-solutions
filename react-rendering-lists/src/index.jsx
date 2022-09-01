ce85421858b8acf8d41c3d06100cb2a0e8755d3d
Monique Chang <moniquechang1997@gmail.com>
2022-09-01 18:32:39 +0000

    edited func component to include props instead of global variable

--- react-rendering-lists/src/index.jsx
+++ react-rendering-lists/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function PokedexList() {
+function PokedexList(props) {
+  const pokedex = props.pokedex;
   const listItems = pokedex.map(x =>
     <li key={x.number}>
       {x.name}
@@ -22,4 +23,4 @@ const pokedex = [
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<PokedexList />);
+root.render(<PokedexList pokedex={pokedex} />);
