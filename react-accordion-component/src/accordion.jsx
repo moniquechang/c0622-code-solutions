56c2ad7fd3b1ef0e02f0a8d4f8fd173b5fbee866
Monique Chang <moniquechang1997@gmail.com>
2022-10-21 20:52:05 +0000

    completed react-accordion-component exercise

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,23 +1,61 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: []
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    const newIsClickedArr = [];
+    this.props.topics.map(topic => newIsClickedArr.push(false));
+    this.setState({ isClicked: newIsClickedArr });
+  }
+
+  createContent() {
+    return (this.props.topics.map((topic, index) => {
+      return (
+        <div key={index}>
+          <h2 id={index} onClick={this.handleClick}>{Object.keys(topic)[0]}</h2>
+          {
+            this.state.isClicked[index] &&
+            <div>
+              <p>{topic[Object.keys(topic)[0]]}</p>
+            </div>
+          }
+          {
+            !this.state.isClicked[index] &&
+            <div className='hidden'>
+              <p>{topic[Object.keys(topic)[0]]}</p>
+            </div>
+          }
+        </div>
+      );
+    }));
+  }
+
+  handleClick(event) {
+    if (!this.state.isClicked[event.target.id]) {
+      const copyArr = this.state.isClicked.map(index => false);
+      copyArr[event.target.id] = true;
+      this.setState({ isClicked: copyArr });
+    } else {
+      const copyArr = this.state.isClicked.map(index => false);
+      this.setState({ isClicked: copyArr });
+    }
+  }
 
   render() {
+    if (this.state.isClicked[0] === undefined) return;
+
     return (
-      <>
-        <div>
-          <h1>HTML</h1>
-          <p>example 1</p>
-        </div>
-        <div>
-          <h1>CSS</h1>
-          <p>example 2</p>
-        </div>
-        <div>
-          <h1>JavaScript</h1>
-          <p>example 3</p>
-        </div>
-      </>
+      <div className='container'>
+        {this.createContent()}
+      </div>
     );
   }
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -5,4 +5,10 @@ import Accordion from './accordion';
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<Accordion />);
+const accordionContent = [
+  { 'Example 1 Topic': 'This is text content for topic example 1' },
+  { 'Example 2 Topic': 'This is text content for topic example 2' },
+  { 'Example 3 Topic': 'This is text content for topic example 3' }
+];
+
+root.render(<Accordion topics={accordionContent} />);
