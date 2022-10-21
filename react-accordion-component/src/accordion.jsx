7934cf6b8c76876f0d04c78c94a7742e895ccbbf
Monique Chang <moniquechang1997@gmail.com>
2022-10-21 06:53:37 +0000

    made accordion layout on render method of accordion class component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -2,4 +2,22 @@ import React from 'react';
 
 export default class Accordion extends React.Component {
 
+  render() {
+    return (
+      <>
+        <div>
+          <h1>HTML</h1>
+          <p>example 1</p>
+        </div>
+        <div>
+          <h1>CSS</h1>
+          <p>example 2</p>
+        </div>
+        <div>
+          <h1>JavaScript</h1>
+          <p>example 3</p>
+        </div>
+      </>
+    );
+  }
 }
