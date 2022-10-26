e2c2e8cd4066f2c0019ddfde111b1746b889baec
Monique Chang <moniquechang1997@gmail.com>
2022-10-26 20:54:35 +0000

    edited isClicked state to be openIndex and used openIndex to render different topics

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -10,25 +10,19 @@ export default class Accordion extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    const newIsClickedArr = [];
-    this.props.topics.map(topic => newIsClickedArr.push(false));
-    this.setState({ isClicked: newIsClickedArr });
-  }
-
   createContent() {
     return (this.props.topics.map((topic, index) => {
       return (
         <div key={index}>
           <h2 id={index} onClick={this.handleClick}>{Object.keys(topic)[0]}</h2>
           {
-            this.state.isClicked[index] &&
+            index === parseInt(this.state.openIndex) &&
             <div>
               <p>{topic[Object.keys(topic)[0]]}</p>
             </div>
           }
           {
-            !this.state.isClicked[index] &&
+            index !== parseInt(this.state.openIndex) &&
             <div className='hidden'>
               <p>{topic[Object.keys(topic)[0]]}</p>
             </div>
@@ -39,19 +33,14 @@ export default class Accordion extends React.Component {
   }
 
   handleClick(event) {
-    if (!this.state.isClicked[event.target.id]) {
-      const copyArr = this.state.isClicked.map(index => false);
-      copyArr[event.target.id] = true;
-      this.setState({ isClicked: copyArr });
+    if (this.state.openIndex === event.target.id) {
+      this.setState({ openIndex: null });
     } else {
-      const copyArr = this.state.isClicked.map(index => false);
-      this.setState({ isClicked: copyArr });
+      this.setState({ openIndex: event.target.id });
     }
   }
 
   render() {
-    if (this.state.isClicked[0] === undefined) return;
-
     return (
       <div className='container'>
         {this.createContent()}
